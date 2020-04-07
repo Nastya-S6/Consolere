52f7cd5ecad92fdd53d2b5ffb9542ffd8a3202e8
Анастасия Саенкова <yfcnz.s_2003@mail.ru>
2020-04-07 10:04:34 +0400

    test

---
+++ Consolere/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Consolere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
